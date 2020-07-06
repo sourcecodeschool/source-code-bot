bd3505c875f61b4fd1357e98a69e8edd725c9f83
unknown <koryakovsky.ivan@gmail.com>
2020-07-06 18:50:41 +0500

    добавлен spring boot

---
+++ src/main/java/ru/javacourse/sourcecodebot/SourceCodeBotApplication.java
@@ -0,0 +1,13 @@
+package ru.javacourse.sourcecodebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceCodeBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourceCodeBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/javacourse/sourcecodebot/SourceCodeBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javacourse.sourcecodebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourceCodeBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
