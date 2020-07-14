35013bb0be00a0f215cf34619614a180077ec108
Zuff <zufarmmm@rambler.ru>
2020-07-14 16:57:09 +0500

    Запись нового пользователя и проверка старого на статус blocked

---
+++ src/main/java/ru/javacourse/sourcecodebot/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package ru.javacourse.sourcecodebot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.javacourse.sourcecodebot.model.Resource;
+import ru.javacourse.sourcecodebot.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
